65762d9a7c1a81c5bebf37c663515da23cd958cd
ubuntu <weihao515@gmail.com>
2020-07-21 05:51:52 +0000

    Edit script

--- 02-script.sh
+++ 02-script.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+#Setup debug flag and source directory
 set -xeu
 cd "$(dirname "$0")"
-#BACKUP_SCRIPT_DIR=$( dirname "$0")
 
-env
+#Setup environment variables
+source export-env.sh
 
-#source $BACKUP_SCRIPT_DIR/export-env.sh
-
-#env
+#Setup variables and backup directory
+DATE=$(date +"%Y%m%d_%H%M%S")
+BACKUP_BASE_DIR=$HOME/backup-storage/
+BACKUP_LOG_DIR=$HOME/backup-log/
+BACKUP_DIR=$HOME/backup-storage/backup-$DATE
+DEBUG_TEXT="\033[1;33m[Backup Script Debug] \033[0m"
+mkdir -p $BACKUP_DIR 
 
 #/usr/local/bin/om version
 #bash $BACKUP_SCRIPT_DIR/export-om-installation.sh
-bash export-om-installation.sh
-$BACKUP_BINARY_DIR/om version
-$BACKUP_BINARY_DIR/om version
+om version
+
+#Run Housekeeping to keep the latest 7 copies of backup artifacts
+ls -d -1t $BACKUP_BASE_DIR/* | tail -n +8 | xargs rm -rvf
+ls -it $BACKUP_LOG_DIR/* | tail -n +8 | xargs rm -rvf
+
